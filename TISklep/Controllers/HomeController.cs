2155b6b519bd958958af9a201a7377912ec576a4
venarim <bartekz441@gmail.com>
2022-03-17 14:16:59 +0100

    Dodanie stron statycznych Zmiany w layoucie

--- TISklep/Controllers/HomeController.cs
+++ TISklep/Controllers/HomeController.cs
@@ -5,19 +5,33 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TISklep.DAL;
 
 namespace TISklep.Controllers
 {
     public class HomeController : Controller
     {
+        MoviesContext db;
+
+        public HomeController(MoviesContext db)
+        {
+            this.db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var kategorie = db.Categories.ToList();
+            return View(kategorie);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult StronyStatyczne(string nazwa)
+        {
+            return View(nazwa);
         }
     }
 }
